66bb5805e503be629a61897d982cfdfb9d24d5c4
joe <joe@izatt.co.uk>
2022-08-10 10:04:45 +0100

    query function

--- app/core/database.php
+++ app/core/database.php
@@ -7,7 +7,26 @@ class Database {
 
 	}
 
-	public function query() {
+	public function query($query,$data = [],$type = 'object') {
 		$con = $this->connect();
+		$stm = $con->prepare($query);
+		if ($stm) {
+			$check = $stm->execute($data);
+			if($check) {
+				$type = PDO::FETCH_OBJ;
+				if ($type != 'object') {
+					$type = PDO::FETCH_ASSOC;
+				}
+
+				$result = $stm->fetchAll($type);
+
+				if ($result && count($result) >0) {
+					return $result;
+				}
+			}
+		}
+
+		return false;
 	}
+
 }

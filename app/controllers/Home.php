dd307f96eec438a62f5829d5102dff7568f65c58
PC <PC@DESKTOP-EF8T872>
2022-08-10 05:30:15 +0100

    Add project files.

---
+++ app/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+class Home {
+	public function index() {
+		echo "home view page";
+	}
+
+	public function edit() {
+		echo "home editing";
+	}
+
+	public function delete() {
+		echo "home delete";
+	}
+}
---
+++ app/controllers/_404.php
@@ -0,0 +1,12 @@
+<?php
+
+
+/**
+* 4040 class page not found
+*/
+
+class _404 {
+	function __construct() {
+		echo "page not found";
+	}
+}

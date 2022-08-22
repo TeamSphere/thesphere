7fa7196407873b517242e8069200b775d88f3174
joe <joe@izatt.co.uk>
2022-08-23 00:10:15 +0100

    Insert function

--- app/controllers/Signup.php
+++ app/controllers/Signup.php
@@ -4,25 +4,18 @@ class Signup extends Controller {
 
 	public function index() {
 
+		show($_POST);
+
 		$user = new User();
 		if($user->validate($_POST)) {
 
-			$query = "insert into users(email,firstname,lastname,password,role,date) values (:email,:firstname,:lastname,:password,:role,:date)";
-			
-			$arr['firstname'] = $_POST['firstname'];
-			$arr['lastname'] = $_POST['lastname'];
-			$arr['email'] = $_POST['email'];
-			$arr['password'] = $_POST['password'];
-			$arr['role'] = "user";
-			$arr['date'] = date("Y-m-d H:i:s");
+			$_POST['date'] = date("Y-m-d H:i:s");
 
-			$db = new Database();
-			$db->query($query,$arr);
+			$user->insert($_POST);
 
 		}
 		
 		show($user->errors);
-		show($_POST);
 		
 		$data['title'] = "Signup";
 
--- app/models/User.php
+++ app/models/User.php
@@ -5,6 +5,17 @@ class User {
 	public $error = [];
 	protected $table = "users";
 
+	protected $allowedColumns = [
+		
+		'email',
+		'firstname',
+		'lastname',
+		'password',
+		'role',
+		'date',
+
+	];
+
 	public function validate($data) {
 
 		$this->errors = [];
@@ -38,5 +49,24 @@ class User {
 		}
 		return false;
 
+	}
+
+	public function insert($data) {
+		// remove unwanted columns
+		if(!empty($this->allowedColumns)) {
+			foreach ($data as $key => $value) {
+				if (!in_array($key, $this->allowedColumns)) {
+					unset($data[$key]);
+				}
+			}
+		}
+
+		$keys = array_keys($data);
+		$values = array_values($data);
+		$query = "insert into users ";
+		$query.= "(".implode(",",$keys) .") values (:".implode(",:",$keys) .")";
+		
+		$db = new Database();
+		$db->query($query,$data);
 	}
 }

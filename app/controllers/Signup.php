c17795b56253ec83844c370931bb39790add6dcc
joe <joe@izatt.co.uk>
2022-08-22 23:42:53 +0100

    Insert into database

--- app/controllers/Signup.php
+++ app/controllers/Signup.php
@@ -5,9 +5,22 @@ class Signup extends Controller {
 	public function index() {
 
 		$user = new User();
-		$result = $user->validate($_POST);
+		if($user->validate($_POST)) {
+
+			$query = "insert into users(email,firstname,lastname,password,role,date) values (:email,:firstname,:lastname,:password,:role,:date)";
+			
+			$arr['firstname'] = $_POST['firstname'];
+			$arr['lastname'] = $_POST['lastname'];
+			$arr['email'] = $_POST['email'];
+			$arr['password'] = $_POST['password'];
+			$arr['role'] = "user";
+			$arr['date'] = date("Y-m-d H:i:s");
+
+			$db = new Database();
+			$db->query($query,$arr);
+
+		}
 		
-		var_dump($result);
 		show($user->errors);
 		show($_POST);
 		
--- app/core/database.php
+++ app/core/database.php
@@ -1,30 +1,40 @@
-<?php
+<?php 
 
-class Database {
-	private function connect() {
+
+/**
+ * database class
+ */
+class Database
+{
+	
+	private function connect()
+	{
 		$str = "mysql:hostname=".DBHOST.";dbname=".DBNAME;
 		return new PDO($str,DBUSER,DBPASS);
 
 	}
 
-	public function query($query,$data = [],$type = 'object') {
+	public function query($query,$data = [],$type = 'object')
+	{
 		$con = $this->connect();
+
 		$stm = $con->prepare($query);
-		if ($stm) {
-
+		if($stm)
+		{
 			$check = $stm->execute($data);
-
-			if($check) {
-
-				if ($type == 'object') {
+			if($check)
+			{
+				if($type == 'object')
+				{
 					$type = PDO::FETCH_OBJ;
-				} else {
+				}else{
 					$type = PDO::FETCH_ASSOC;
 				}
 
 				$result = $stm->fetchAll($type);
 
-				if ($result && count($result) >0) {
+				if(is_array($result) && count($result) > 0)
+				{
 					return $result;
 				}
 			}
@@ -32,28 +42,31 @@ class Database {
 
 		return false;
 	}
-	
-	public function create_tables() {
+
+	public function create_tables()
+	{
 		//users table
 		$query = "
 
 			CREATE TABLE IF NOT EXISTS `users` (
-				`id` int(11) NOT NULL AUTO_INCREMENT,
-				`email` varchar(100) NOT NULL,
-				`firstname` varchar(30) NOT NULL,
-				`lastname` varchar(30) NOT NULL,
-				`password` varchar(255) NOT NULL,
-				`role` varchar(20) NOT NULL,
-				`date` date DEFAULT NULL,
-				PRIMARY KEY (`id`),
-				KEY `email` (`email`),
-				KEY `firstname` (`firstname`),
-				KEY `lastname` (`lastname`),
-				KEY `date` (`date`)
+			 `id` int(11) NOT NULL AUTO_INCREMENT,
+			 `email` varchar(100) NOT NULL,
+			 `firstname` varchar(30) NOT NULL,
+			 `lastname` varchar(30) NOT NULL,
+			 `password` varchar(255) NOT NULL,
+			 `role` varchar(20) NOT NULL,
+			 `date` date DEFAULT NULL,
+			 PRIMARY KEY (`id`),
+			 KEY `email` (`email`),
+			 KEY `firstname` (`email`),
+			 KEY `lastname` (`email`),
+			 KEY `date` (`date`)
 			) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 
 		";
 
 		$this->query($query);
-		}
 	}
+
+	
+}

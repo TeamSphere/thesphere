7274ce40dadd18d1a74f22270bb0e15a8719017a
joe <joe@izatt.co.uk>
2022-08-23 01:04:20 +0100

    Model class

--- app/models/User.php
+++ app/models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-class User {
+class User extends Model {
 
 	public $error = [];
 	protected $table = "users";
@@ -27,10 +27,17 @@ class User {
 		if(empty($data['lastname'])) {
 			$this->errors['lastname'] = "A last name is required";
 		}
-
-		if(empty($data['email'])) {
-			$this->errors['emaile'] = "An email name is required";
+		// check email
+		$query = "select * from users where email = :email limit 1";
+		if(!filter_var($data['email'],FILTER_VALIDATE_EMAIL)) {
+			$this->errors['email'] = "Email is not valid";
+		} else {
+			if($this->query($query,['email'=>$data['email']])) {
+				$this->errors['lastname'] = "That email already exists";
+			}
 		}
+
+		
 
 		if(empty($data['password'])) {
 			$this->errors['password'] = "A password is required";
@@ -51,22 +58,5 @@ class User {
 
 	}
 
-	public function insert($data) {
-		// remove unwanted columns
-		if(!empty($this->allowedColumns)) {
-			foreach ($data as $key => $value) {
-				if (!in_array($key, $this->allowedColumns)) {
-					unset($data[$key]);
-				}
-			}
-		}
-
-		$keys = array_keys($data);
-		$values = array_values($data);
-		$query = "insert into users ";
-		$query.= "(".implode(",",$keys) .") values (:".implode(",:",$keys) .")";
-		
-		$db = new Database();
-		$db->query($query,$data);
-	}
+	
 }
